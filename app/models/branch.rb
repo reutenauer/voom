ef5f319c567cc9423c17cb7f0b46fb10a3660113
Arthur Reutenauer <arthur.reutenauer@roh.org.uk>
2015-06-27 04:01:39 +0100

    Doesn’t quite work yet.

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -12,24 +12,37 @@ class Branch
     branch = "voom-#{t}"
     FileUtils.chdir 'server-configuration---content'
     `git checkout -b #{branch}`
+
     conffile = 'etc/apache2/sites-available/default.conf'
     oldconf = File.read(conffile)
     newconf = File.open(conffile, 'w')
+
+    prevoom = ""
     oldconf.each_line do |line|
-      newconf.puts line
+      prevoom += line
+      prevoom += "\n"
       break if line =~ /# ==== Managed by the Voom below ====/
     end
 
     oldconf.each_line do |line|
       break if line =~ /# ==== Managed by the Voom above ====/
     end
-    Redirect.all.each do |redirect|
-      newconf.puts "        Redirect 302 #{redirect.source}   #{redirect.target}"
-    end
 
+    postvoom = ""
     oldconf.each_line do |line|
+      postvoom += line
+      postvoom += "\n"
       newconf.puts line
     end
+
+    newconf.write prevoom
+    Redirect.all.each do |redirect|
+      l = redirect.source.length
+      padding = if l < 23 then ' ' * (23 - l) else ' ' end
+      puts "DEBUG #{redirect.source} #{redirect.source.length}"
+      newconf.puts "        Redirect 302 #{redirect.source}#{padding}#{redirect.target}"
+    end
+    newconf.write postvoom
 
     # conf = File.open(conffile, 'w')
     # conf.puts "        Redirect 301 #{source}   #{target}"
