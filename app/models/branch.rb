b1eb3a586122af3408286d46a1ec72acc578bde1
Arthur Reutenauer <arthur.reutenauer@roh.org.uk>
2015-06-27 02:57:48 +0100

    Getting a bit lost here.

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -12,10 +12,30 @@ class Branch
     branch = "voom-#{t}"
     FileUtils.chdir 'server-configuration---content'
     `git checkout -b #{branch}`
-    conf = File.open('etc/apache2/sites-available/default.conf', 'w')
+    conffile = 'etc/apache2/sites-available/default.conf'
+    oldconf = File.read(conffile)
+    prevoom = ""
+    oldconf.each_line do |line|
+      prevoom += line
+      prevoom += "\n"
+      break if line =~ /# ==== Managed by the Voom below ====/
+    end
+    oldconf.each_line do |line|
+      break if line =~ /# ==== Managed by the Voom above ====/
+    end
+    Redirect.all.each do |redirect|
+      newconf.puts "        Redirect 302 #{source}   #{target}"
+    end
+    postvoom = ""
+    oldconf.each_line do |line|
+      postvoom += line
+      postvoom += "\n"
+    end
+    conf = File.open(conffile, 'w')
     conf.puts "        Redirect 301 #{source}   #{target}"
     conf.close
+    system("git diff")
     `git commit -m 'Commit from the Voom.'`
-    puts `git push -u origin #{branch}`
+    # puts `git push -u origin #{branch}`
   end
 end
