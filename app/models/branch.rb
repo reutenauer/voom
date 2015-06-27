fe4c06e180bed511c9ac4ea4c4dcc8d927c27a39
Arthur Reutenauer <arthur.reutenauer@roh.org.uk>
2015-06-27 02:42:17 +0100

    Making the new branch – almost there.

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -5,10 +5,17 @@ class Branch
   @@tmpdir = Dir.mktmpdir
 
   def self.add_vanity_url(source, target)
+    puts @@tmpdir
     FileUtils.chdir @@tmpdir
-    `git clone ssh://git@stash.atlassian.roh.org.uk/web/server-configuration---content.git`
+    `git clone -b voom-base ssh://git@stash.atlassian.roh.org.uk/web/server-configuration---content.git`
+    t = Time.now.utc.strftime('%Y%m%dT%H%M%S')
+    branch = "voom-#{t}"
     FileUtils.chdir 'server-configuration---content'
+    `git checkout -b #{branch}`
     conf = File.open('etc/apache2/sites-available/default.conf', 'w')
     conf.puts "        Redirect 301 #{source}   #{target}"
+    conf.close
+    `git commit -m 'Commit from the Voom.'`
+    puts `git push -u origin #{branch}`
   end
 end
