6821405baac5a04f1dd56700b01b35ad2d6f6168
Arthur Reutenauer <arthur.reutenauer@roh.org.uk>
2015-06-27 15:52:08 +0100

    Normalising database - new headings table.

---
+++ db/migrate/20150627140831_normalise_redirect_heading.rb
@@ -0,0 +1,16 @@
+class NormaliseRedirectHeading < ActiveRecord::Migration
+  def change
+    remove_column :redirects, :heading, :string
+
+    change_table :redirects do |t|
+      t.references :headings
+    end
+
+    create_table :headings do |t|
+      t.string :title
+      t.integer :rank
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150627032018) do
+ActiveRecord::Schema.define(version: 20150627140831) do
+
+  create_table "headings", force: :cascade do |t|
+    t.string  "title"
+    t.integer "rank"
+  end
 
   create_table "redirects", force: :cascade do |t|
     t.string   "source"
     t.string   "target"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "heading"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "headings_id"
   end
 
 end
