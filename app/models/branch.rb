b7fcfa9bf341cf44fff7dcb7dc757db3ee6733cb
Arthur Reutenauer <arthur.reutenauer@roh.org.uk>
2015-06-27 03:00:13 +0100

    Think I’m back on track.

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -14,26 +14,26 @@ class Branch
     `git checkout -b #{branch}`
     conffile = 'etc/apache2/sites-available/default.conf'
     oldconf = File.read(conffile)
-    prevoom = ""
+    newconf = File.open(conffile, 'w')
     oldconf.each_line do |line|
-      prevoom += line
-      prevoom += "\n"
+      newconf.puts line
       break if line =~ /# ==== Managed by the Voom below ====/
     end
+
     oldconf.each_line do |line|
       break if line =~ /# ==== Managed by the Voom above ====/
     end
     Redirect.all.each do |redirect|
-      newconf.puts "        Redirect 302 #{source}   #{target}"
+      newconf.puts "        Redirect 302 #{redirect.source}   #{redirect.target}"
     end
-    postvoom = ""
+
     oldconf.each_line do |line|
-      postvoom += line
-      postvoom += "\n"
+      newconf.puts line
     end
-    conf = File.open(conffile, 'w')
-    conf.puts "        Redirect 301 #{source}   #{target}"
-    conf.close
+
+    # conf = File.open(conffile, 'w')
+    # conf.puts "        Redirect 301 #{source}   #{target}"
+    # conf.close
     system("git diff")
     `git commit -m 'Commit from the Voom.'`
     # puts `git push -u origin #{branch}`
