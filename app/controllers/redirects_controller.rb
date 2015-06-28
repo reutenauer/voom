ec7cf398cf54265e8404c4a8878718a8e6b57dbe
Arthur Reutenauer <arthur.reutenauer@roh.org.uk>
2015-06-28 19:12:34 +0100

    More wip

--- app/controllers/redirects_controller.rb
+++ app/controllers/redirects_controller.rb
@@ -5,6 +5,7 @@ class RedirectsController < ApplicationController
 
   def create
     @redirect = Redirect.new(params[:redirect].permit(:source, :target, :name, :stakeholder).merge({ heading: Heading.find_by_title("added by the Voom"), type: Type.find_by_status_code(301) }))
+    @already_exists = Redirect.find_by_source(@redirect.source)
     @branch_name = Branch.new.add_vanity_url(@redirect)
   end
 
