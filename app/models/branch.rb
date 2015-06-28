cf45bea43cab17ae79f31fd6e7e294ba0854979e
Arthur Reutenauer <arthur.reutenauer@roh.org.uk>
2015-06-28 18:07:37 +0100

    Making add_vanity_url an instance method to have different temporary directories.

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -2,15 +2,18 @@ require 'tmpdir'
 require 'fileutils'
 
 class Branch
-  @@tmpdir = Dir.mktmpdir
   PREVOOM = 0
   INVOOM = 1
   POSTVOOM = 2
 
-  def self.add_vanity_url(redirect)
+  def initialize
+    @tmpdir = Dir.mktmpdir
+  end
+
+  def add_vanity_url(redirect)
     redirect.save!
-    puts @@tmpdir
-    FileUtils.chdir @@tmpdir
+    puts @tmpdir
+    FileUtils.chdir @tmpdir
     `git clone -b voom-base ssh://git@stash.atlassian.roh.org.uk/web/server-configuration---content.git`
     t = Time.now.utc.strftime('%Y%m%dT%H%M%S')
     branch = "voom-#{t}"
@@ -70,7 +73,7 @@ class Branch
     # conf = File.open(conffile, 'w')
     # conf.puts "        Redirect 301 #{source}   #{target}"
     # conf.close
-    # system("git diff")
+    system("git diff")
     `git commit -m 'Commit from the Voom.'`
     # puts `git push -u origin #{branch}`
     branch
--- spec/models/branch_spec.rb
+++ spec/models/branch_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Branch do
-  describe ".create" do
+  describe "#create" do
     # TODO
     # it "checks out the git repository" do
     #   expect do
@@ -10,8 +10,9 @@ describe Branch do
     # end
 
     it "works" do
+      branch = Branch.new
       redirect = Redirect.new(source: "/flyingpigs", target: "http://www.roh.org.uk/productions/the-flying-pigs-by-ebenezer-scrooge", name: "My name", stakeholder: "The Big Cheese", heading: Heading.find_by_title("added by the Voom"), type: Type.find_by_status_code(301))
-      Branch.add_vanity_url(redirect)
+      branch.add_vanity_url(redirect)
     end
   end
 end
